4fe5bb058baa6cb16648f11a36b864304f82107d
omar zarate <alberich_sid@hotmail.com>
2017-01-15 15:30:42 -0600

    usamos el procesador de plantillas de phpword

--- plantilla.php
+++ plantilla.php
@@ -4,7 +4,8 @@
 require_once dirname(__FILE__).'/PHPWord-master/src/PhpWord/Autoloader.php';
 \PhpOffice\PhpWord\Autoloader::register();
 
-
+//le decimos a phpword que vamos a usar el procesador de plantillas
+use PhpOffice\PhpWord\TemplateProcessor;
 
 
 
