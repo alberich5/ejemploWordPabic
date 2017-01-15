4dfc9146356d98109506b6c408bf9e0da7614fb4
omar zarate <alberich_sid@hotmail.com>
2017-01-15 15:32:15 -0600

    asignamos ubicacion de la plantila a usar

--- plantilla.php
+++ plantilla.php
@@ -6,8 +6,8 @@ require_once dirname(__FILE__).'/PHPWord-master/src/PhpWord/Autoloader.php';
 
 //le decimos a phpword que vamos a usar el procesador de plantillas
 use PhpOffice\PhpWord\TemplateProcessor;
-
-
+//guardamos un objecto de templateprocessor y asinamos ubicacion de la plantilla
+$templateWord = new TemplateProcessor('ofi3.docx');
 
 
 
