b974fa92b32f74df43098d3128dbf1bd513a9132
omar zarate <alberich_sid@hotmail.com>
2017-01-15 15:34:25 -0600

    creamos la variables que remplazaremos en la plantilla

--- plantilla.php
+++ plantilla.php
@@ -8,8 +8,11 @@ require_once dirname(__FILE__).'/PHPWord-master/src/PhpWord/Autoloader.php';
 use PhpOffice\PhpWord\TemplateProcessor;
 //guardamos un objecto de templateprocessor y asinamos ubicacion de la plantilla
 $templateWord = new TemplateProcessor('ofi3.docx');
-
-
+//variables de prueba que rellenan la plantilla
+$nombre = "SANDRA SIERRA LOPEZ";
+$edad = "35";
+$direccion = "LOS MOCHIS SINALOA";
+$estado_civil="soltero"
 
 
  ?>
