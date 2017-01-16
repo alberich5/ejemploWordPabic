17f006ec318e9814bd9003b744cc30a319f8e351
Omar zarate Hipolito <alberich_sid@hotmail.com>
2017-01-16 12:41:23 -0600

    se creo una nueva plantilla para generar las altas de los elementos

---
+++ plantillalta.php
@@ -0,0 +1,34 @@
+<?php 
+
+//agregamos la ruta de phpword
+require_once dirname(__FILE__).'/PHPWord-master/src/PhpWord/Autoloader.php';
+\PhpOffice\PhpWord\Autoloader::register();
+
+//le decimos a phpword que vamos a usar el procesador de plantillas
+use PhpOffice\PhpWord\TemplateProcessor;
+//guardamos un objecto de templateprocessor y asinamos ubicacion de la plantilla
+
+$templateWord = new TemplateProcessor('ofi2.docx');
+//variables de prueba que rellenan la plantilla
+$nombre = "SANDRA SIERRA LOPEZ";
+$oficio = "35";
+$fecha = "1 de enero del 2017";
+
+
+
+// --- Asignamos valores a la plantilla
+$templateWord->setValue('nombre',$nombre);
+$templateWord->setValue('oficio',$oficio);
+$templateWord->setValue('fecha',$fecha);
+
+//variables
+
+
+// --- Guardamos el documento
+$templateWord->saveAs('Contrato.docx');
+//agregamos la cabecera para descargar el documentos desde php
+header("Content-Disposition: attachment; filename=Contrato.docx; charset=iso-8859-1");
+echo file_get_contents('Contrato.docx');
+
+
+?>
